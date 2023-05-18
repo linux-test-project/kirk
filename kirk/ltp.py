011a7d9ace1f5673c673ea2c720eb17862e9f816
Andrea Cervesato <andrea.cervesato@suse.com>
2023-05-18 19:09:16 +0200

    Add LTP Framework implementation
    LTP Framework implementation is used to communicate with Linux Test
    Project framework and it implements internal folders handling to run
    its tests.

---
+++ kirk/ltp.py
@@ -0,0 +1,197 @@
+"""
+.. module:: ltp
+    :platform: Linux
+    :synopsis: LTP framework definition
+
+.. moduleauthor:: Andrea Cervesato <andrea.cervesato@suse.com>
+"""
+import os
+import json
+import logging
+from kirk import KirkException
+from kirk.sut import SUT
+from kirk.data import Suite
+from kirk.data import Test
+from kirk.framework import Framework
+
+
+class LTPFramework(Framework):
+    """
+    Linux Test Project framework definition.
+    """
+
+    PARALLEL_BLACKLIST = [
+        "needs_root",
+        "needs_device",
+        "mount_device",
+        "mntpoint",
+        "resource_file",
+        "format_device",
+        "save_restore",
+        "max_runtime"
+    ]
+
+    def __init__(self) -> None:
+        self._logger = logging.getLogger("ltp")
+        self._root = "/opt/ltp"
+        self._env = {
+            "LTPROOT": self._root,
+            "TMPDIR": "/tmp",
+            "LTP_COLORIZE_OUTPUT": "1",
+        }
+
+    def setup(self, **kwargs: dict) -> None:
+        env = kwargs.get("env", None)
+        if env:
+            self._env.update(env)
+
+        root = kwargs.get("root", None)
+        if root:
+            self._root = root
+            self._env["LTPROOT"] = self._root
+
+    # pylint: disable=too-many-locals
+    def _read_runtest(
+            self,
+            suite_name: str,
+            content: str,
+            metadata: dict = None) -> Suite:
+        """
+        It reads a runtest file content and it returns a Suite object.
+        :param suite_name: name of the test suite
+        :type suite_name: str
+        :param content: content of the runtest file
+        :type content: str
+        :param metadata: metadata JSON file content
+        :type metadata: dict
+        :returns: Suite
+        """
+        self._logger.info("collecting testing suite: %s", suite_name)
+
+        metadata_tests = None
+        if metadata:
+            self._logger.info("Reading metadata content")
+            metadata_tests = metadata.get("tests", None)
+
+        tests = []
+        lines = content.split('\n')
+        tc_path = os.path.join(self._root, "testcases", "bin")
+
+        for line in lines:
+            if not line.strip() or line.strip().startswith("#"):
+                continue
+
+            self._logger.debug("Test declaration: %s", line)
+
+            parts = line.split()
+            if len(parts) < 2:
+                raise KirkException(
+                    "runtest file is not defining test command")
+
+            test_name = parts[0]
+            test_cmd = parts[1]
+            test_args = []
+
+            if len(parts) >= 3:
+                test_args = parts[2:]
+
+            parallelizable = True
+
+            if not metadata_tests:
+                # no metadata no party
+                parallelizable = False
+            else:
+                test_params = metadata_tests.get(test_name, None)
+                if test_params:
+                    self._logger.info(
+                        "Found %s test params in metadata", test_name)
+                    self._logger.debug("params=%s", test_params)
+
+                if test_params is None:
+                    # this probably means test is not using new LTP API,
+                    # so we can't decide if test can run in parallel or not
+                    parallelizable = False
+                else:
+                    for blacklist_param in self.PARALLEL_BLACKLIST:
+                        if blacklist_param in test_params:
+                            parallelizable = False
+                            break
+
+            if not parallelizable:
+                self._logger.info("Test '%s' is not parallelizable", test_name)
+            else:
+                self._logger.info("Test '%s' is parallelizable", test_name)
+
+            test = Test(
+                name=test_name,
+                cmd=test_cmd,
+                args=test_args,
+                cwd=tc_path,
+                env={
+                    "LTPROOT": self._root,
+                    "TMPDIR": "/tmp",
+                    "LTP_COLORIZE_OUTPUT": "1",
+                },
+                parallelizable=parallelizable)
+
+            tests.append(test)
+
+            self._logger.debug("test: %s", test)
+
+        self._logger.debug("Collected tests: %d", len(tests))
+
+        suite = Suite(suite_name, tests)
+
+        self._logger.debug(suite)
+        self._logger.info("Collected testing suite: %s", suite_name)
+
+        return suite
+
+    @property
+    def name(self) -> str:
+        return "ltp"
+
+    async def get_suites(self, sut: SUT) -> list:
+        if not sut:
+            raise ValueError("SUT is None")
+
+        runtest_dir = os.path.join(self._root, "runtest")
+        ret = await sut.run_command(f"test -d {runtest_dir}")
+        if ret["returncode"] != 0:
+            raise KirkException(f"'{runtest_dir}' doesn't exist inside SUT")
+
+        ret = await sut.run_command(f"ls --format=single-column {runtest_dir}")
+        stdout = ret["stdout"]
+        if ret["returncode"] != 0:
+            raise KirkException(f"command failed with: {stdout}")
+
+        suites = [line for line in stdout.split('\n') if line]
+        return suites
+
+    async def find_suite(self, sut: SUT, name: str) -> Suite:
+        if not sut:
+            raise ValueError("SUT is None")
+
+        if not name:
+            raise ValueError("name is empty")
+
+        suite_path = os.path.join(self._root, "runtest", name)
+        if not os.path.isfile(suite_path):
+            raise KirkException(f"'{name}' suite doesn't exist")
+
+        metadata_path = os.path.join(
+            self._root,
+            "metadata",
+            "ltp.json"
+        )
+
+        metadata = None
+        if os.path.isfile(metadata_path):
+            with open(metadata_path, 'r', encoding='utf-8') as metdata_f:
+                metadata = json.loads(metdata_f.read())
+
+        data = await sut.fetch_file(suite_path)
+        content = data.decode(encoding="utf-8", errors="ignore")
+        suite = self._read_runtest(name, content, metadata)
+
+        return suite
---
+++ kirk/tests/test_ltp.py
@@ -0,0 +1,128 @@
+"""
+Test Framework implementations.
+"""
+import os
+import json
+import pytest
+from kirk.ltp import LTPFramework
+from kirk.host import HostSUT
+
+pytestmark = pytest.mark.asyncio
+
+
+class TestLTPFramework:
+    """
+    Inherit this class to implement framework tests.
+    """
+    TESTS_NUM = 6
+    SUITES_NUM = 3
+
+    @pytest.fixture
+    async def sut(self):
+        """
+        Host SUT communication object.
+        """
+        obj = HostSUT()
+        obj.setup()
+
+        await obj.communicate()
+        yield obj
+        await obj.stop()
+
+    @pytest.fixture
+    def ltp(self, tmpdir):
+        """
+        LTP framework object.
+        """
+        fw = LTPFramework()
+        fw.setup(root=str(tmpdir))
+
+        yield fw
+
+    @pytest.fixture(autouse=True)
+    def prepare_tmpdir(self, tmpdir):
+        """
+        Prepare the temporary directory adding runtest folder.
+        """
+        # create simple testing suites
+        content = ""
+        for i in range(self.TESTS_NUM):
+            content += f"test0{i} echo ciao\n"
+
+        tmpdir.mkdir("testcases").mkdir("bin")
+        runtest = tmpdir.mkdir("runtest")
+
+        for i in range(self.SUITES_NUM):
+            suite = runtest / f"suite{i}"
+            suite.write(content)
+
+        # create a suite that is executing slower than the others
+        # and it's parallelizable
+        content = ""
+        for i in range(self.TESTS_NUM, self.TESTS_NUM * 2):
+            content += f"slow_test0{i} sleep 0.05\n"
+
+        suite = runtest / f"slow_suite"
+        suite.write(content)
+
+        tests = {}
+        for i in range(self.TESTS_NUM, self.TESTS_NUM * 2):
+            name = f"slow_test0{i}"
+            tests[name] = {}
+
+        metadata_d = {"tests": tests}
+        metadata = tmpdir.mkdir("metadata") / "ltp.json"
+        metadata.write(json.dumps(metadata_d))
+
+    def test_name(self, ltp):
+        """
+        Test that name property is not empty.
+        """
+        assert ltp.name == "ltp"
+
+    async def test_get_suites(self, ltp, sut, tmpdir):
+        """
+        Test get_suites method.
+        """
+        suites = await ltp.get_suites(sut)
+        assert "suite0" in suites
+        assert "suite1" in suites
+        assert "suite2" in suites
+        assert "slow_suite" in suites
+
+    async def test_find_suite(self, ltp, sut, tmpdir):
+        """
+        Test find_suite method.
+        """
+        for i in range(self.SUITES_NUM):
+            suite = await ltp.find_suite(sut, f"suite{i}")
+            assert len(suite.tests) == self.TESTS_NUM
+
+            for j in range(self.TESTS_NUM):
+                test = suite.tests[j]
+                assert test.name == f"test0{j}"
+                assert test.command == "echo"
+                assert test.arguments == ["ciao"]
+                assert test.cwd == os.path.join(
+                    str(tmpdir),
+                    "testcases",
+                    "bin")
+                assert not test.parallelizable
+                assert "LTPROOT" in test.env
+                assert "TMPDIR" in test.env
+                assert "LTP_COLORIZE_OUTPUT" in test.env
+
+        suite = await ltp.find_suite(sut, "slow_suite")
+        assert len(suite.tests) == self.TESTS_NUM
+
+        for test in suite.tests:
+            assert test.command == "sleep"
+            assert test.arguments == ["0.05"]
+            assert test.cwd == os.path.join(
+                str(tmpdir),
+                "testcases",
+                "bin")
+            assert test.parallelizable
+            assert "LTPROOT" in test.env
+            assert "TMPDIR" in test.env
+            assert "LTP_COLORIZE_OUTPUT" in test.env
