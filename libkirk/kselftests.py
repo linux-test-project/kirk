bb14a02fe689c1ce2c6b4fbc2ae0efac1b5e9753
Andrea Cervesato <andrea.cervesato@suse.com>
2023-06-27 14:55:31 +0200

    Revert "Read kselftests cgroup shell tests"
    This reverts commit 3a9c04415e910063691ce776a20361b571e55c2e.

--- libkirk/kselftests.py
+++ libkirk/kselftests.py
@@ -50,17 +50,13 @@ class KselftestFramework(Framework):
             raise KirkException(
                 f"cgroup folder is not available: {cgroup_dir}")
 
-        names = []
-        for ext in ("c", "sh"):
-            ret = await sut.run_command(
-                f"basename -s .c -- test_*.{ext}",
-                cwd=cgroup_dir)
-            if ret["returncode"] != 0 or not ret["stdout"]:
-                raise KirkException("Can't read cgroup tests")
+        ret = await sut.run_command(
+            "basename -s .c -- test_*.c",
+            cwd=cgroup_dir)
+        if ret["returncode"] != 0 or not ret["stdout"]:
+            raise KirkException("Can't read cgroup tests")
 
-            tests = ret["stdout"].split('\n')
-            names.extend(tests)
-
+        names = ret["stdout"].split('\n')
         tests_obj = []
 
         for name in names:
