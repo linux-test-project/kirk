aa7d693cd5a090fe6d5dad01e0899ac85f51442f
Andrea Cervesato <andrea.cervesato@suse.com>
2024-07-26 12:04:25 +0200

    Refactor setuptools script
    This patch entroduces README in the long_description and reformat
    setuptools script for better reading.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- setup.py
+++ setup.py
@@ -4,39 +4,43 @@
    :synopsis: installer module
 .. moduleauthor:: Andrea Cervesato <andrea.cervesato@mailbox.org>
 """
-import libkirk
+
 from setuptools import setup
 
+import libkirk
+
 setup(
-    name='kirk',
+    name="kirk",
     version=libkirk.__version__,
-    description='All-in-one Linux Testing Framework',
-    author='Andrea Cervesato',
-    author_email='andrea.cervesato@mailbox.org',
-    license='GPLv2',
-    url='https://github.com/acerv/kirk',
+    description="All-in-one Linux Testing Framework",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Andrea Cervesato",
+    author_email="andrea.cervesato@mailbox.org",
+    license="GPLv2",
+    url="https://github.com/acerv/kirk",
     classifiers=[
-        'Natural Language :: English',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Topic :: Software Development :: Testing',
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Testing",
     ],
     extras_require={
-        'ssh':  ['asyncssh <= 2.13.2'],
-        'ltx':  ['msgpack <= 1.0.5'],
+        "ssh": ["asyncssh <= 2.13.2"],
+        "ltx": ["msgpack <= 1.0.5"],
     },
-    packages=['libkirk'],
+    packages=["libkirk"],
     include_package_data=True,
     entry_points={
-        'console_scripts': [
-            'kirk=libkirk.main:run',
+        "console_scripts": [
+            "kirk=libkirk.main:run",
         ],
     },
 )
