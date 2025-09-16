27dc2c0dbf43ccb6d97d25ccbc559f3d39526f05
Andrea Cervesato <andrea.cervesato@suse.com>
2025-09-16 09:07:05 +0200

    Make plugins cloneable
    Add clone() method to Plugin API and extend it to the plugins
    implementations. Add libkirk.com.clone_channel() to follow this pattern
    and to clone new channels inside the application.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- libkirk/tests/test_plugin.py
+++ libkirk/tests/test_plugin.py
@@ -2,10 +2,29 @@
 Unittests for framework module.
 """
 
+import pytest
 import libkirk
 import libkirk.plugin
+from libkirk.plugin import Plugin
 from libkirk.framework import Framework
 
+
+@pytest.fixture(autouse=True)
+def setup(tmpdir):
+    """
+    Setup the temporary folder before tests.
+    """
+    plugins = []
+    plugins.append(tmpdir / "pluginA.py")
+    plugins.append(tmpdir / "pluginB.py")
+    plugins.append(tmpdir / "pluginC.txt")
+
+    for index in range(0, len(plugins)):
+        plugins[index].write(
+            "from libkirk.plugin import Plugin\n\n"
+            "class MyPlugin(Plugin):\n"
+            "    _name = 'myplug'\n"
+        )
 
 
 def test_framework(tmpdir):
@@ -29,3 +48,13 @@ def test_framework(tmpdir):
     suts = libkirk.plugin.discover(Framework, str(tmpdir))
 
     assert len(suts) == 2
+
+
+def test_clone(tmpdir):
+    """
+    Test if ``clone`` method properly forks inside ``Plugin``.
+    """
+    plugins = libkirk.plugin.discover(Plugin, str(tmpdir))
+    newplugin = plugins[0].clone("myclone")
+
+    assert newplugin.name == "myclone"
