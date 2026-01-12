7abb21f8c15618c75d5e4cf6570da772dac99b34
Andrea Cervesato <andrea.cervesato@suse.com>
2026-01-12 10:41:40 +0100

    tests: revert test_session dummy framework
    Qemu and SSH (on remote target) tests are failing because we create an
    LTP installation dummy folder in our local machine instead of the
    target. This patch reverts Dummyframework as it was in the commit
    818abbb.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- libkirk/tests/test_session.py
+++ libkirk/tests/test_session.py
@@ -2,11 +2,18 @@
 Unittests for the session module.
 """
 
-import asyncio
+import os
 import json
+import pathlib
+import asyncio
+from typing import List
 
 import pytest
 
+from libkirk.ltp import LTPFramework
+from libkirk.com import ComChannel
+from libkirk.data import Test, Suite
+from libkirk.results import TestResults
 from libkirk.session import Session
 from libkirk.tempfile import TempDir
 
@@ -19,17 +26,163 @@ async def sut():
     raise NotImplementedError()
 
 
+class DummyFramework(LTPFramework):
+    """
+    A generic framework created for testing.
+    """
+
+    async def get_suites(self, channel: ComChannel) -> List[str]:
+        return ["suite01", "suite02", "sleep", "environ", "kernel_panic"]
+
+    async def find_command(self, channel: ComChannel, command: str) -> Test:
+        return Test(name=command, cmd=command)
+
+    async def find_suite(self, channel: ComChannel, name: str) -> Suite:
+        if name in "suite01":
+            test0 = Test(
+                name="test01",
+                cwd=self._root,
+                env=self._env,
+                cmd="echo",
+                args=["-n", "ciao0"],
+                parallelizable=False,
+            )
+
+            test1 = Test(
+                name="test02",
+                cwd=self._root,
+                env=self._env,
+                cmd="echo",
+                args=["-n", "ciao0"],
+                parallelizable=False,
+            )
+
+            return Suite(name, [test0, test1])
+        if name == "suite02":
+            test0 = Test(
+                name="test01",
+                cwd=self._root,
+                env=self._env,
+                cmd="echo",
+                args=["-n", "ciao0"],
+                parallelizable=False,
+            )
+
+            test1 = Test(
+                name="test02",
+                cwd=self._root,
+                env=self._env,
+                cmd="sleep",
+                args=["0.2", "&&", "echo", "-n", "ciao1"],
+                parallelizable=True,
+            )
+
+            return Suite(name, [test0, test1])
+        elif name == "sleep":
+            test0 = Test(
+                name="test01",
+                cwd=self._root,
+                env=self._env,
+                cmd="sleep",
+                args=["2"],
+                parallelizable=False,
+            )
+
+            test1 = Test(
+                name="test02",
+                cwd=self._root,
+                env=self._env,
+                cmd="sleep",
+                args=["2"],
+                parallelizable=False,
+            )
+
+            return Suite(name, [test0, test1])
+        elif name == "environ":
+            test0 = Test(
+                name="test01",
+                cwd=self._root,
+                env=self._env,
+                cmd="echo",
+                args=["-n", "$hello"],
+                parallelizable=False,
+            )
+
+            return Suite(name, [test0])
+        else:
+            test0 = Test(
+                name="test01",
+                cwd=self._root,
+                env=self._env,
+                cmd="echo",
+                args=["Kernel", "panic"],
+                parallelizable=False,
+            )
+
+            test1 = Test(
+                name="test01",
+                cwd=self._root,
+                env=self._env,
+                cmd="sleep",
+                args=["0.2"],
+                parallelizable=False,
+            )
+
+            return Suite(name, [test0, test1])
+
+    async def read_result(
+        self, test: Test, stdout: str, retcode: int, exec_t: float
+    ) -> TestResults:
+        passed = 0
+        failed = 0
+        skipped = 0
+        broken = 0
+        skipped = 0
+        warnings = 0
+        error = retcode == -1
+
+        if retcode == 0:
+            passed = 1
+        elif retcode == 4:
+            warnings = 1
+        elif retcode == 32:
+            skipped = 1
+        elif not error:
+            failed = 1
+
+        if error:
+            broken = 1
+
+        result = TestResults(
+            test=test,
+            passed=passed,
+            failed=failed,
+            broken=broken,
+            skipped=skipped,
+            warnings=warnings,
+            exec_time=exec_t,
+            retcode=retcode,
+            stdout=stdout,
+        )
+
+        return result
+
+
 class _TestSession:
     """
     Test for Session class.
     """
 
     @pytest.fixture
-    async def session(self, ltpdir, sut):
+    async def session(self, tmpdir, sut):
         """
         Session communication object.
         """
-        session = Session(tmpdir=TempDir(str(ltpdir)), sut=sut)
+        # make sure that ltp folder is present inside the host
+        pathlib.Path("/opt/ltp").mkdir(parents=True, exist_ok=True)
+
+        session = Session(tmpdir=TempDir(tmpdir), sut=sut)
+        session._framework = DummyFramework()
         yield session
         await asyncio.wait_for(session.stop(), timeout=30)
 
