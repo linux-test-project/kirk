dccf0c8790302ab095e1a4b73e7d4c3fa8709790
Andrea Cervesato <andrea.cervesato@suse.com>
2026-02-21 12:35:39 +0100

    plugin: optimize code
    - make use of internal pathlib methods
    - more pythonic code
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- libkirk/plugin.py
+++ libkirk/plugin.py
@@ -10,6 +10,7 @@ import importlib
 import importlib.util
 import inspect
 import os
+from pathlib import Path
 from typing import (
     Any,
     Dict,
@@ -89,25 +90,26 @@ def discover(mytype: type, folder: str) -> List[Plugin]:
 
     loaded_obj = []
 
-    for myfile in os.listdir(folder):
-        if not myfile.endswith(".py"):
+    # use pathlib for more efficient iteration
+    folder_path = Path(folder)
+
+    for py_file in folder_path.glob("*.py"):
+        if not py_file.is_file():
             continue
 
-        path = os.path.join(folder, myfile)
-        if not os.path.isfile(path):
-            continue
-
-        spec = importlib.util.spec_from_file_location("obj", path)
+        spec = importlib.util.spec_from_file_location("obj", str(py_file))
         if not spec or not spec.loader:
             continue
 
         module = importlib.util.module_from_spec(spec)
         spec.loader.exec_module(module)
 
+        module_name = module.__name__
+
         members = inspect.getmembers(module, inspect.isclass)
         for _, klass in members:
             if (
-                klass.__module__ != module.__name__
+                klass.__module__ != module_name
                 or klass is mytype
                 or klass in loaded_obj
             ):
@@ -116,7 +118,7 @@ def discover(mytype: type, folder: str) -> List[Plugin]:
             if issubclass(klass, mytype):
                 loaded_obj.append(klass())
 
-    if len(loaded_obj) > 0:
+    if loaded_obj:
         loaded_obj.sort(key=lambda x: x.name)
 
     return loaded_obj
